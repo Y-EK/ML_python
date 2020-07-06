ffab5b898f6b3399c683cab4ee659e5bc49bafb8
Youness <elkhatabi.youness@gmail.com>
2020-07-06 18:42:32 +0200

    add example of application of the K-NN algorithm to 12 random images

--- KNN_script.py
+++ KNN_script.py
@@ -6,7 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn import neighbors
 
 # load data 
-mnist = fetch_mldata('MNIST original', data_home='E:/y_elk/Documents/PROJETS_GIT/ML_python/mnist')
+#mnist = fetch_mldata('MNIST original', data_home='E:/y_elk/Documents/PROJETS_GIT/ML_python/mnist')
+mnist = fetch_mldata('MNIST original', data_home='./mnist')
 
 #
 #print(mnist.data.shape)
@@ -40,4 +41,33 @@ for k in range(2,15):
     knn = neighbors.KNeighborsClassifier(k)
     errors.append(100*(1 - knn.fit(x_train, y_train).score(x_test, y_test)))
 plt.plot(range(2,15), errors, 'o-')
+plt.show()
+
+# Get the value of n_neighbors corresponding to a minimal error 
+ymin = min(errors)
+xpos = errors.index(ymin)
+xmin = range(2,15)[xpos]
+
+# Get the best performing classifier
+knn = neighbors.KNeighborsClassifier(xmin)
+knn.fit(x_train, y_train)
+
+# Get the predictions of the test data
+predicted = knn.predict(x_test)
+
+# We resize the data in the form of images
+images = x_test.reshape((-1, 28, 28))
+
+# Select a sample of 12 random images
+select = np.random.randint(images.shape[0], size=12)
+
+# Display the images with the associated prediction
+fig,ax = plt.subplots(3,4)
+
+for index, value in enumerate(select):
+    plt.subplot(3,4,index+1)
+    plt.axis('off')
+    plt.imshow(images[value],cmap=plt.cm.gray_r,interpolation="nearest")
+    plt.title('Predicted: {}'.format( predicted[value]) )
+
 plt.show()
