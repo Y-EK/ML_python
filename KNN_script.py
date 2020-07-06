aacb9a811a14bd1cb829030f2611d57d57388fd6
Youness <elkhatabi.youness@gmail.com>
2020-07-06 17:23:29 +0200

    add measurement of performance of the model

--- KNN_script.py
+++ KNN_script.py
@@ -1,7 +1,9 @@
 # libraries
 from sklearn.datasets import fetch_mldata
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn import neighbors
 
 # load data 
 mnist = fetch_mldata('MNIST original', data_home='E:/y_elk/Documents/PROJETS_GIT/ML_python/mnist')
@@ -19,3 +21,23 @@ target = mnist.target[sample]
 
 # create training and test set
 x_train, x_test, y_train, y_test = train_test_split(data, target, train_size=0.8)
+
+#model = neighbors.KNeighborsClassifier(n_neighbors=3)
+#model.fit(x_train, y_train)
+#print(model.predict([x_test[3]]))
+#print(y_test[3])
+# Percent error of the model applied to test data
+#print(1 - model.score(x_test, y_test))
+
+# Plot percent error in terms of number of neighbors
+# Set x axis label 
+plt.xlabel('n_neighbors')
+# Set y axis label 
+plt.ylabel('Percent error')
+
+errors = []
+for k in range(2,15):
+    knn = neighbors.KNeighborsClassifier(k)
+    errors.append(100*(1 - knn.fit(x_train, y_train).score(x_test, y_test)))
+plt.plot(range(2,15), errors, 'o-')
+plt.show()
